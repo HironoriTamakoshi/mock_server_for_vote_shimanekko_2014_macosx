1b17cd0ede9ec76eda029084a689e9e856de5fed
HironoriTamakoshi <tmks0828@gmail.com>
2014-10-28 13:49:10 +0900

    途中

--- tcp-server.rb
+++ tcp-server.rb
@@ -1,22 +1,41 @@
 require "socket"
 
-#ポート番号8000でopen
-server = TCPServer.open(8000);
-while true
-  #クライアントからの接続を待つ
-  puts "クライアントからの接続を待ちます"
-  sock = server.accept
-  #接続相手の情報を得る
-  puts "接続相手の情報:[アドレスファミリ, port番号, ホストを表す文字列, IPアドレス]"
-  p sock.peeraddr
-  #クライアントからのデータを最後まで受信する
-  #受信したデータはコンソールに表示される
-  while buf = sock.gets
-    p buf
-  end
-  #クライアントとの接続ソケットを閉じる
-  sock.close
-end
+#ルートの設定
+DOCUMENT_ROOT = File.expand_path("../", __FILE__)
+MESSAGE =<<EOF
+HTTP/1.1 200 OK
+Content-Type: test/html; charset=UTF-8
+Server: tama
+Transfer-Encoding: chunked
+Connection: close
 
-#待ち受けソケットを閉じる
-server.close
+EOF
+
+begin
+  #ポート番号8000でopen
+  server = TCPServer.open(8000);
+
+  while true
+    #クライアントからの接続を待つ
+    puts "クライアントからの接続を待ちます"
+    Thread.start(server.accept) do |socket|        
+      while(request = socket.gets)
+        break if request.include? "END"
+        p request
+        #リクエストGETに対してhtmlを返す
+        if request.include? "GET"
+          content = File.open("#{DOCUMENT_ROOT}/test.html"){ |f| f.read }
+          socket.write (MESSAGE+content)
+        end
+      end
+      #クライアントとの接続ソケットを閉じる
+      sock.close
+    end
+  end
+
+  #待ち受けソケットを閉じる
+  server.close
+rescue => e
+  p e
+  p e.backtrace
+end
